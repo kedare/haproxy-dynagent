7956636a43e188f594eeef2bc6cfcf1ad8c53262
Mathieu Poussin <mathieu@lodgify.com>
2017-02-02 12:29:51 +0100

    Start service after install

--- install.ps1
+++ install.ps1
@@ -2,3 +2,4 @@ $pwd = Get-Location
 $servicePath = "${pwd}\haproxy-dynagent.exe"
 Write-Host "Registering service for ${servicePath}"
 New-Service -Name "HAProxyDynAgent" -DisplayName "HAProxy DynAgent" -BinaryPathName $servicePath -StartupType Automatic -Description "Manage backend state on HAProxy"
+Start-Service HAProxyDynAgent
